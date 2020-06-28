4139832d371f4c6fa03d751c567bc0fea598ebe4
Mayan Atir <mayanfigura@gmail.com>
2020-06-28 10:12:46 +0300

    Updates

---
+++ precache-manifest.afd2ed8eb3badd928ec2e427e0269dd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc54c8d04e2cba75f368890cc016f91e",
+    "url": "/charts/index.html"
+  },
+  {
+    "revision": "9111273cc2d16556a4b8",
+    "url": "/charts/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9324e0c806ebff6c3c52",
+    "url": "/charts/static/js/2.f052067a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/charts/static/js/2.f052067a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9111273cc2d16556a4b8",
+    "url": "/charts/static/js/main.32b7d0a6.chunk.js"
+  },
+  {
+    "revision": "0b5ce14b27a82fe9cf6a",
+    "url": "/charts/static/js/runtime-main.89bb55ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/charts/static/media/logo.5d5d9eef.svg"
+  }
+]);
