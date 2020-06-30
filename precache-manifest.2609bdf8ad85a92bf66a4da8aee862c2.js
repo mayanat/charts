0cd775e934c8f579caecfe37d04b83847bacb91b
Mayan Atir <mayanfigura@gmail.com>
2020-06-30 14:27:26 +0300

    Updates

--- precache-manifest.2609bdf8ad85a92bf66a4da8aee862c2.js
+++ precache-manifest.2609bdf8ad85a92bf66a4da8aee862c2.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "364538d786773522589c8125dabb6aa3",
+    "revision": "7f229d22cd4534398be3391f0fb50008",
     "url": "/charts/index.html"
   },
   {
-    "revision": "7153e6db252e71c4d64f",
+    "revision": "a458ba0ecbccfadf7f09",
     "url": "/charts/static/css/2.829c9cb5.chunk.css"
   },
   {
-    "revision": "676417f888ac917d2af6",
+    "revision": "5eb3efeea4f34037be3a",
     "url": "/charts/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "7153e6db252e71c4d64f",
-    "url": "/charts/static/js/2.75d6a406.chunk.js"
+    "revision": "a458ba0ecbccfadf7f09",
+    "url": "/charts/static/js/2.6526e1df.chunk.js"
   },
   {
     "revision": "fcca2cdcc5e6967026a1ba00172f2d47",
-    "url": "/charts/static/js/2.75d6a406.chunk.js.LICENSE.txt"
+    "url": "/charts/static/js/2.6526e1df.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "676417f888ac917d2af6",
-    "url": "/charts/static/js/main.76f05679.chunk.js"
+    "revision": "5eb3efeea4f34037be3a",
+    "url": "/charts/static/js/main.e77c7977.chunk.js"
   },
   {
     "revision": "0b5ce14b27a82fe9cf6a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/charts/precache-manifest.9c9262a6b329d560e285af2041d5d316.js"
+  "/charts/precache-manifest.2609bdf8ad85a92bf66a4da8aee862c2.js"
 );
 
 self.addEventListener('message', (event) => {
